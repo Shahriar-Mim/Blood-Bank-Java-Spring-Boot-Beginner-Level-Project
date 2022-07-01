1746068adf6ea68bea3964a1023c347528af199d
Shahriar-Mim <shahriar.mim01@gmail.com>
2022-07-01 20:51:10 +0600

    first commit

---
+++ src/main/java/com/bloodrepository/BloodBankRepositoryApplication.java
@@ -0,0 +1,15 @@
+package com.bloodrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankRepositoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BloodBankRepositoryApplication.class, args);
+    }
+
+}
+
+
+
---
+++ src/main/java/com/bloodrepository/User.java
@@ -0,0 +1,104 @@
+package com.bloodrepository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true, length = 45)
+	private String email;
+	
+	@Column(nullable = false, length = 64)
+	private String password;
+	
+	@Column(name = "first_name", nullable = false, length = 20)
+	private String firstName;
+	
+	@Column(name = "last_name", nullable = false, length = 20)
+	private String lastName;
+
+	@Column(name = "blood_group", nullable = false, length = 20)
+	private String bloodGroup;
+
+	@Column(name = "address", nullable = false)
+	private String address;
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Column(name = "phone_number", length = 11)
+	private String phoneNumber;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getBloodGroup() {
+		return bloodGroup;
+	}
+
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
